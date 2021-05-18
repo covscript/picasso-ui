11587ed8970e96e1d961b41c874fd0a565e9b808
李登淳 <lee@covariant.cn>
2021-05-18 21:45:52 +0800

    update windows

--- run.bat
+++ run.bat
@@ -4,5 +4,6 @@ mkdir imports
 call ecs -o .\imports\ -c picasso_base.ecs
 call ecs -o .\imports\ -c picasso_activity.ecs
 call ecs -o .\imports\ -c picasso_widget.ecs
+call ecs -o .\imports\ -c picasso_window.ecs
 call ecs -o .\imports\ -c picasso.ecs
 call ecs -- "-i .\imports" .\test.ecs
