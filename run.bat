f4d1a7a53fedaffecb2af6ba0b25b6c88532047c
李登淳 <mikecovlee@163.com>
2023-04-01 01:14:15 +0800

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,9 +1,10 @@
 @echo off
 cd %~dp0
 mkdir imports
-call ecs -o .\imports\ -c picasso_base.ecs
-call ecs -o .\imports\ -c picasso_activity.ecs
-call ecs -o .\imports\ -c picasso_widget.ecs
-call ecs -o .\imports\ -c picasso_window.ecs
-call ecs -o .\imports\ -c picasso.ecs
-call ecs -- "-i .\imports" .\test.ecs
+set ECS_FLAGS=-g
+call ecs -o .\imports\ %ECS_FLAGS% picasso_base.ecs
+call ecs -o .\imports\ %ECS_FLAGS% picasso_activity.ecs
+call ecs -o .\imports\ %ECS_FLAGS% picasso_widget.ecs
+call ecs -o .\imports\ %ECS_FLAGS% picasso_window.ecs
+call ecs -o .\imports\ %ECS_FLAGS% picasso.ecs
+call ecs -i .\imports\ %ECS_FLAGS% test.ecs
